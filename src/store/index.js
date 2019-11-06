2e7165353e95ab98076d8203860d62ec8889d603
Filip Jerga <jerga99@gmail.com>
2019-11-06 09:08:18 +0100

    19 Login and Register page

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import Vuex from 'vuex'
 import { vuexfireMutations } from 'vuexfire'
 
 import exchange from './modules/exchange'
+import auth from './modules/auth'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    exchange
+    exchange,
+    auth
   },
   actions: {
     test() {
