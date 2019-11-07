c4043b543213c8a40499c7fb2b1def2a4d3d3d01
Filip Jerga <jerga99@gmail.com>
2019-11-07 10:13:35 +0100

    29 Protect Pages

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ import LoginPage from '@/pages/Login'
 import RegisterPage from '@/pages/Register'
 import ProfilePage from '@/pages/Profile'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
 Vue.use(Router)
 
 const router = new Router({
@@ -31,21 +34,64 @@ const router = new Router({
     {
       path: '/login',
       name: 'LoginPage',
-      component: LoginPage
+      component: LoginPage,
+      meta: { onlyGuestUser: true }
     },
     {
       path: '/register',
       name: 'RegisterPage',
-      component: RegisterPage
+      component: RegisterPage,
+      meta: { onlyGuestUser: true }
     },
     {
       path: '/users/me',
       name: 'ProfilePage',
-      component: ProfilePage
+      component: ProfilePage,
+      meta: { onlyAuthUser: true }
     }
 	],
   mode: 'history'
 })
+
+
+router.beforeEach((to, from, next) => {
+  const authenticatedUser = firebase.auth().currentUser
+  debugger
+
+  if (to.meta.onlyAuthUser) {
+    if (authenticatedUser) {
+      next()
+    } else {
+      // TODO: Navigate to page for non authenticated users
+      next({name: 'LoginPage'})
+    }
+  } else if (to.meta.onlyGuestUser) {
+    if (authenticatedUser) {
+      next({name: 'HomePage'})
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default router
 
