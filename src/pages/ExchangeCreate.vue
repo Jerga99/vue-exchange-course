9e9f68c5291d11f48dbf55b9960f771c41eb0fd3
Filip Jerga <jerga99@gmail.com>
2019-11-07 17:59:02 +0100

    36 Handle Create exchange form submit

--- src/pages/ExchangeCreate.vue
+++ src/pages/ExchangeCreate.vue
@@ -6,9 +6,9 @@
           <label class="label">Type</label>
           <div class="control">
             <div class="select">
-              <select>
-                <option>Service</option>
-                <option>Product</option>
+              <select v-model="form.type">
+                <option value="service">Service</option>
+                <option value="product">Product</option>
               </select>
             </div>
           </div>
@@ -16,44 +16,72 @@
         <div class="field">
           <label class="label">Title</label>
           <div class="control">
-            <input class="input" type="text" placeholder="Text input">
+            <input 
+              v-model="form.title"
+              class="input" 
+              type="text" 
+              placeholder="Some Nice Product">
           </div>
         </div>
         <div class="field">
           <label class="label">Description</label>
           <div class="control">
-            <textarea class="textarea" placeholder="Textarea"></textarea>
+            <textarea 
+              v-model="form.description"
+              class="textarea" 
+              placeholder="Some catchy description about product">
+            </textarea>
           </div>
         </div>
         <div class="field">
           <label class="label">Image Link</label>
           <div class="control">
-            <input class="input" type="text" placeholder="Text input">
+            <input 
+              v-model="form.image"
+              class="input" 
+              type="text" 
+              placeholder="https://unsplash....">
           </div>
         </div>
         <!-- TODO: Rename To Price -->
         <div class="field">
-          <label class="label">Value</label>
+          <label class="label">Price</label>
           <div class="control">
-            <input class="input" type="text" placeholder="Text input">
+            <input 
+              v-model="form.price"
+              class="input" 
+              type="number" 
+              placeholder="249">
           </div>
         </div>
         <div class="field">
           <label class="label">Country</label>
           <div class="control">
-            <input class="input" type="text" placeholder="Text input">
+            <input 
+              v-model="form.country"
+              class="input" 
+              type="text" 
+              placeholder="Slovakia">
           </div>
         </div>
         <div class="field">
           <label class="label">City</label>
           <div class="control">
-            <input class="input" type="text" placeholder="Text input">
+            <input 
+              v-model="form.city"
+              class="input" 
+              type="text" 
+              placeholder="Bratislava">
           </div>
         </div>
 
+        <!-- TODO: provide tags inputs -->
+
         <div class="field is-grouped">
           <div class="control">
-            <button class="button is-link">Submit</button>
+            <button
+              @click.prevent="createExchange"
+              class="button is-link">Submit</button>
           </div>
           <div class="control">
             <button class="button is-text">Cancel</button>
@@ -63,8 +91,28 @@
     </div>
   </div>
 </template>
-
 <script>
+export default {
+  data() {
+    return {
+      form: {
+        title: '',
+        description: '',
+        type: 'service',
+        image: '',
+        price: null,
+        country: '',
+        city: '',
+        tags: []
+      }
+    }
+  },
+  methods: {
+    createExchange() {
+      alert(JSON.stringify(this.form))
+    }
+  }
+}
 </script>
 
 
@@ -74,3 +122,21 @@
   margin: 0 auto;
 }
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
