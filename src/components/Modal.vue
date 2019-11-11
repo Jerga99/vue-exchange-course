7fcef0e0d291c02ee79b62870436ee6930e9f7ac
Filip Jerga <jerga99@gmail.com>
2019-11-11 09:59:18 +0100

    47 Reusable Modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <button 
+      @click="isOpen = true" 
+      class="button is-primary is-outlined m-t-sm">Update Info</button>
+    <div :class="['modal', {'is-active': isOpen}]">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">User Profile</p>
+          <button
+            @click="isOpen = false"
+            class="delete"
+            aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <slot />
+        </section>
+        <footer class="modal-card-foot">
+          <button 
+            @click="submitModal"
+            class="button is-success">Save changes</button>
+          <button @click="isOpen = false" class="button">Cancel</button>
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      onModalSubmit: {
+        required: true,
+        type: Function
+      }
+    },
+    data () {
+      return {
+        isOpen: false
+      }
+    },
+    methods: {
+      submitModal() {
+        this.onModalSubmit(() => this.isOpen = false)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
+
+
+
+
+
+
+
+
+
--- src/components/profile/UserUpdateModal.vue
+++ src/components/profile/UserUpdateModal.vue
@@ -1,71 +1,51 @@
 <template>
-  <div>
-    <button 
-      @click="isOpen = true" 
-      class="button is-primary is-outlined m-t-sm">Update Info</button>
-    <div :class="['modal', {'is-active': isOpen}]">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">User Profile</p>
-          <button
-            @click="isOpen = false"
-            class="delete"
-            aria-label="close"></button>
-        </header>
-        <section class="modal-card-body">
-          <form>
-            <div class="field">
-              <label class="title">Full Name</label>
-              <input 
-                v-model="profileToUpdate.fullName"
-                class="input">
-            </div>
-            <div class="field">
-              <label class="title">Address</label>
-              <input 
-                v-model="profileToUpdate.address"
-                class="input">
-            </div>
-            <div class="field">
-              <label class="title">Country</label>
-              <input 
-                v-model="profileToUpdate.country"
-                class="input">
-            </div>
-            <div class="field">
-              <label class="title">Phone</label>
-              <input 
-                v-model="profileToUpdate.phone"
-                class="input">
-            </div>
-            <div class="field">
-              <label class="title">Avatar</label>
-              <input 
-                v-model="profileToUpdate.avatar"
-                class="input">
-            </div>
-            <div class="field">
-              <label class="title">Info about user</label>
-              <input 
-                v-model="profileToUpdate.info"
-                class="input">
-            </div>
-          </form>
-        </section>
-        <footer class="modal-card-foot">
-          <button 
-            @click="submitModal"
-            class="button is-success">Save changes</button>
-          <button @click="isOpen = false" class="button">Cancel</button>
-        </footer>
+  <app-modal 
+    :onModalSubmit="submitModal">
+    <form>
+      <div class="field">
+        <label class="title">Full Name</label>
+        <input 
+          v-model="profileToUpdate.fullName"
+          class="input">
       </div>
-    </div>
-  </div>
+      <div class="field">
+        <label class="title">Address</label>
+        <input 
+          v-model="profileToUpdate.address"
+          class="input">
+      </div>
+      <div class="field">
+        <label class="title">Country</label>
+        <input 
+          v-model="profileToUpdate.country"
+          class="input">
+      </div>
+      <div class="field">
+        <label class="title">Phone</label>
+        <input 
+          v-model="profileToUpdate.phone"
+          class="input">
+      </div>
+      <div class="field">
+        <label class="title">Avatar</label>
+        <input 
+          v-model="profileToUpdate.avatar"
+          class="input">
+      </div>
+      <div class="field">
+        <label class="title">Info about user</label>
+        <input 
+          v-model="profileToUpdate.info"
+          class="input">
+      </div>
+    </form>
+  </app-modal>
 </template>
 
 <script>
+  import AppModal from '@/components/Modal'
   export default {
+    components: { AppModal },
     props: {
       userProfile: {
         required: true,
@@ -78,15 +58,12 @@
     },
     data () {
       return {
-        isOpen: false,
         profileToUpdate: {...this.userProfile}
       }
     },
     methods: {
-      submitModal() {
-        this.onModalSubmit({...this.profileToUpdate}, () => {
-          this.isOpen = false
-        })
+      submitModal(closeCallback) {
+        this.onModalSubmit({...this.profileToUpdate}, closeCallback)
       }
     }
   }
